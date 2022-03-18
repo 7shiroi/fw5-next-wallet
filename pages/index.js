4ef3103854c9631a5687b8ef2200fd00f7d6e0de
7shiroi <lucky7kelvin@gmail.com>
2022-03-18 10:32:46 +0800

    feat: implement menubar to home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Image from 'next/image'
 import { Col, Container, Row } from 'react-bootstrap'
 import Footer from '../components/Footer'
 import MenuBar from '../components/MenuBar'
+import Button from '../components/Button'
 import NavbarComponent from '../components/NavbarComponent'
+import {BsPlusLg} from 'react-icons/bs'
+import {AiOutlineArrowUp} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -15,13 +18,43 @@ export default function Home() {
       </Head>
 
       <NavbarComponent />
-      <Container>
+      <Container className='py-4'>
         <Row>
           <Col md={3}>
             <MenuBar />
           </Col>
           <Col md={9}>
-
+            <Row>
+              <Container className='d-flex flex-column flex-sm-row justify-content-between primary-bg'>
+                <div className='d-flex flex-column justify-content-around py-4'>
+                  <div>
+                    Balance
+                  </div>
+                  <h1>
+                    Rp. {Number(120000).toLocaleString('id-ID')}
+                  </h1>
+                  <div>
+                    +62 813-9387-7946
+                  </div>
+                </div>
+                <div className='d-flex flex-row flex-sm-column justify-content-around pb-4 pb-sm-0'>
+                  <Button variant="primary">
+                    <AiOutlineArrowUp /> Transfer
+                  </Button>
+                  <Button variant="primary">
+                    <BsPlusLg /> Top Up
+                  </Button>
+                </div>
+              </Container>
+            </Row>
+            <Row>
+              <Col md={7}>
+              {/* add chart */}
+              </Col>
+              <Col md={5}>
+              {/* add history */}
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
