67885dead407e9869c4f89889ff8099599c4f26b
7shiroi <lucky7kelvin@gmail.com>
2022-03-18 16:12:11 +0800

    feat:history

--- components/HistoryDashboard.js
+++ components/HistoryDashboard.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import Styles from '../styles/HistoryDashboard.module.css'
 import Image from 'next/image'
+import { Col, Row } from 'react-bootstrap'
 
 const HistoryDashboard = ({image, name, type, nominal, ...rest}) => {
   return (
-    <div className='d-flex justify-content-between'>
-      <div className='d-flex'>
-        <Image width={56} height={56} src={image} alt={name} />
-        <div className='d-flex flex-column ms-3'>
-          <div>
-            {name}
-          </div>
-          <div>
-            <small>{type}</small>
-          </div>
-        </div>
-      </div>
-      <div className={`${type==='Transfer' ? Styles.redInfo : Styles.greenInfo} d-flex align-items-center`}>
+    <Row>
+      <Col xs={8}>
+        <Row>
+          <Col xs={4}>
+            <Image width={56} height={56} src={image} alt={name} />
+          </Col>
+          <Col xs={8} className='d-flex flex-column'>
+            <div className='text-truncate'>
+              {name}
+            </div>
+            <div>
+              <small>{type}</small>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+      <Col xs={4} className={`${type==='Transfer' ? Styles.redInfo : Styles.greenInfo} d-flex align-items-center`}>
         {type==='Transfer' ? '-' : '+'}Rp. {Number(nominal).toLocaleString('id-ID')}
-      </div>
-    </div>
+      </Col>
+    </Row>
   )
 }
 
